a93085fdf635c7f811ba56cd6f6b4b53800bca4b
lwx <l@w.x>
2020-10-18 20:13:11 +0800

    Auto Commit.

---
+++ precache-manifest.a7e91cb3ecd4acdf6b2001d3a48331c9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65e7995ee9e542cd2e037d1479219992",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ed1bd4cd2e3b42fa912",
+    "url": "/static/css/main.49790f4b.chunk.css"
+  },
+  {
+    "revision": "44ab1b202e18f1b817cd",
+    "url": "/static/js/2.9ffc951f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.9ffc951f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ed1bd4cd2e3b42fa912",
+    "url": "/static/js/main.2814e604.chunk.js"
+  },
+  {
+    "revision": "dd1b78d4627008c63f67",
+    "url": "/static/js/runtime-main.7e054cd6.js"
+  },
+  {
+    "revision": "a21423c523fb17bade7a89fb2c6392b0",
+    "url": "/static/media/01_Android Studio.a21423c5.pdf"
+  },
+  {
+    "revision": "b58479cffc423dfbbda28d36fde9f69d",
+    "url": "/static/media/02_The Android System.b58479cf.pdf"
+  },
+  {
+    "revision": "7737f13aa727708099b4bf61c8322758",
+    "url": "/static/media/03_Layouts, Resources and Permissions.7737f13a.pdf"
+  },
+  {
+    "revision": "2ae9d83f62c6af2a3f6168b0b435fb48",
+    "url": "/static/media/04_Activity and Intent.2ae9d83f.pdf"
+  },
+  {
+    "revision": "5798686f2e3e650758dc4040def7bdd0",
+    "url": "/static/media/05_UI Controls.5798686f.pdf"
+  },
+  {
+    "revision": "e7b3bb4448ba368e1635b92d1fdd34fd",
+    "url": "/static/media/06_Data Persistence.e7b3bb44.pdf"
+  },
+  {
+    "revision": "1a74f105d59bc8934a3fce5d4d991d6c",
+    "url": "/static/media/07_Fragment.1a74f105.pdf"
+  },
+  {
+    "revision": "8cf024c57551a1dd3ff88438dd75f543",
+    "url": "/static/media/08_Threads and AsyncTask.8cf024c5.pdf"
+  },
+  {
+    "revision": "7cbb43b03b0f4967ed18187bf8dba7bf",
+    "url": "/static/media/09_Notification.7cbb43b0.pdf"
+  },
+  {
+    "revision": "3c3e5887260072261c6b3ec9c5755b9b",
+    "url": "/static/media/10_Services.3c3e5887.pdf"
+  },
+  {
+    "revision": "1c7c9874eee818fdd88aa69b8cff251b",
+    "url": "/static/media/11_Content Provider.1c7c9874.pdf"
+  },
+  {
+    "revision": "4e1cb69c84fdb0db65c079b291874b52",
+    "url": "/static/media/12_Broadcast Receiver.4e1cb69c.pdf"
+  }
+]);
